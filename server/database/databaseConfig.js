1a01f9758f8d9b7214a1b5cfbe1daf5eaae8bb7c
Carol A. Nikolic <carolnikolic@gmail.com>
2024-08-14 14:17:10 +0200

    fix: fix db setup and include variables.

--- server/database/databaseConfig.js
+++ server/database/databaseConfig.js
@@ -1,12 +1,25 @@
 import pg from "pg";
+import env from "dotenv";
 
-const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "bookList",
-    password: "Bazingadatabase$",
-    port: 5432,
-    application_name: "todoApp"
+env.config();
+
+const pool = new pg.Pool({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DB,
+    password: process.env.PG_PASS,
+    port: process.env.PG_PORT,
+    application_name: process.env.PG_APP_NAME
 });
 
-export default db
+// Test the connection
+pool.connect((err, client, release) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.stack);
+    } else {
+        console.log('Connected to the database');
+    }
+    release(); 
+});
+
+export default pool;
